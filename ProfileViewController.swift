80592c1a0244b4dcdff854b7e49a368266b3c235
Sparky314 <sergey.davlitshin@gmail.com>
2022-03-11 23:26:18 +0200

    HW finished

---
+++ ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Sergey on 07.03.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+        
+    @IBOutlet weak var profile: ProfileView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
